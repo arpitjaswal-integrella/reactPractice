f09eafc9f9969995329b85f301bd39b747feac0a
arpitjaswal-integrella <arpit.jaswal@integrella.com>
2023-02-14 14:30:13 +0530

    quick progress update

---
+++ ReactRouter/reactrouter/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import {Home} from "./components/Home"
+import {About} from "./components/About"
+import { NavBar } from "./components/NavBar";
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+    </Routes>
+    </>
+  );
+}
+
+export default App;
